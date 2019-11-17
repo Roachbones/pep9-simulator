f9fc2ea602fa4feec56dab8d5ac6c894ee228ee8
Kevin Thomas <26861054+Roachbones@users.noreply.github.com>
2019-11-17 13:13:36 -0500

    started step 1

--- ConsoleApplication2/ConsoleApplication2.c
+++ ConsoleApplication2/ConsoleApplication2.c
@@ -1,33 +1,9 @@
-/*
-Kevin Thomas
-9/17/2019
-CSCI 220
-2.c
-
-Takes two integers a and b as input.
-Demonstrates the output of five mathematical operations on them.
-*/
 #define _CRT_SECURE_NO_WARNINGS
 #include <stdio.h>
 
 int main()
-{
-	//very confusing
-	int a;
-	int b;
-	printf("a: ");
-	scanf("%d", &a);
-	printf("b: ");
-	scanf("%d", &b);
-	printf("%d + %d = %d\n", a, b, a + b);
-	printf("%d - %d = %d\n", a, b, a - b);
-	printf("%d * %d = %d\n", a, b, a * b);
-	if (b == 0) {
-		printf("Can't divide by zero.\n");
-	}
-	else {
-		printf("%d / %d = %d\n", a, b, a / b);
-		printf("%d %% %d = %d\n", a, b, a % b);
-	}
-	printf("All done.\n");
+{	
+	printf("Starting simulator...\n\n");
+	int Mem[65536]; // Pep/9 main memory
+	printf("This is where the simulation would go, but we haven't gotten that far yet.");
 }
